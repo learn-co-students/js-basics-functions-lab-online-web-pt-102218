56cc9836c1bf491cca7e1989cfc587f4278dd234
github username <github email address>
2019-05-15 22:25:12 -0500

    Done.

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 function distanceFromHqInBlocks (fromPoint) {
     //returns the number of blocks given a value
-    let distance
     if(fromPoint > 42) {
-        distance = fromPoint - 42;
-    }else if(fromPoint < 42){
-        distance = 42 - fromPoint;
+       return fromPoint - 42;
+    }else {
+        return 42 - fromPoint;
     }
-    return distance;
 }
 
    
@@ -15,7 +13,7 @@ function distanceFromHqInFeet (someValue) {
     // calls distanceFromHqInBlocks from inside the distanceFromHqInFeet function, passing the argument from distanceFromHqInFeet into distanceFromHqInBlocks
     //the return value of distanceFromHqInBlocks can then be used to calculate feet
 }
-  
+
 function distanceTravelledInFeet(startBlock, endBlock){
     let distance 
     if(startBlock > endBlock) {
@@ -27,16 +25,15 @@ function distanceTravelledInFeet(startBlock, endBlock){
 }
 
 function calculatesFarePrice(start, destination) {
-    let price
-    if (distanceTravelledInFeet(start, destination) <= 400){
-        price = 0;
-    } else if (distanceTravelledInFeet(start, destination) > 2500) {
-        price = 'cannot travel that far'
-    }else if (distanceTravelledInFeet(start, destination) > 2000){
-        price = 25;
-    } else if (400 < distanceTravelledInFeet(start, destination) < 2000){ 
-        price = (distanceTravelledInFeet(start, destination) * 2) / 100 - 8;
+    let distance = distanceTravelledInFeet(start, destination)
+    if (distance <= 400){
+        return 0;
+    } else if (distance > 400 && distance <= 2000){ 
+        return (distance - 400) * 0.02;
+    }else if (distance > 2000 && distance < 2500){
+        return 25;
+    } else {
+        return 'cannot travel that far';
     }
-    return price ;
 
 }
